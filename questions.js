eafca6b8360c5b10eb58f35290a3461166ef44e1
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-05-21 16:30:10 -0600

    added files

---
+++ questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Which method removes the last element from an array and returns that element in Javascript?',
+    choices: ['last()', 'get()', 'pop()', 'None of the above.'],
+    answer: 'pop()',
+  },
+  {
+    title: 'How does a for loop start?',
+    choices: ['for (i = 0; i <= 10; i++)', 'for (i = 0; i <= 10)', 'for i = 5 to 10', 'for (i <= xx.length; i++)'  ],
+    answer:'for (i = 0; i <= 10; i++)',
+  },
+  {
+    title: 'Inside what HTML tag you would put JavaScript code?',
+    choices: ['<js>', '<scripting>', '<javascript>', '<script>'],
+    answer:'<script>',
+  },
+  {
+    title: 'How do you declare a JavaScript variable x?',
+    choices: ['def x', 'var x', 'variable x', 'define x'],
+    answer: 'var x',
+  },
+  {
+    title: 'What will return of the Boolean(100 > 1)',
+    choices: ["Undefined", 'NaN', 'true', 'false'],
+    answer: "true",
+  }
+];
